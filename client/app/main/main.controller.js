7e0bfd84434dcd8abc69304e7f98cc5196a90c75
Una Gauper <umgauper@gmail.com>
2015-07-11 17:20:51 -0700

    set up route for /datas that sends search request via Yelp's API

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -7,14 +7,11 @@ angular.module('webstormProjectsApp')
 
         // get data via yelp's search API
 
-        var url = 'http://api.yelp.com/v2/search?term=bar&location=';
-            url = url + $scope.user.location; // will need to turn spaces btwn words into '+' for two or three word places
+        $http.get('api/datas') //later we'll send location as parameter as {location: $scope.user.location};
 
-        $http.get(url).success(function(data) {
-
-            console.log(data);
-
-        });
+          .success(function(data) {
+            $scope.bars = data.businesses; //the business array
+          });
 
 
       }
