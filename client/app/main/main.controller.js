44a301a76d705cb6bc0e4c7757bc65fbd0ee7c8a
Una Gauper <umgauper@gmail.com>
2015-07-13 10:17:26 -0700

    added endpoint for places

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,17 +3,30 @@
 angular.module('webstormProjectsApp')
   .controller('MainCtrl', function ($scope, $http) {
 
+      //$scope.location = '';
+
+      $scope.user = 'Test User 1'; //later after Twitter auth working, change to Auth.getCurrentUser()
+
       $scope.search = function() {
+
+        // get data from api/places
+
+        $http.get('api/places/' + $scope.location.toLowerCase());
 
         // get data via yelp's search API
 
-        $http.get('api/datas/' + $scope.user.location)
+        $http.get('api/datas/' + $scope.location)
 
           .success(function(data) {
             $scope.bars = data.businesses; //the business array
           });
 
 
-      }
+      };
+
+      $scope.addMe = function(location, place) {
+        location = location.toLowerCase();
+        $http.post('api/places', {location: location, place: place, users: [$scope.user]});
+      };
 
   });
---
+++ server/api/place/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var express = require('express');
+var controller = require('./place.controller');
+
+var router = express.Router();
+
+router.get('/', controller.index);
+//router.get('/:id', controller.show);
+router.get('/:location', controller.showPlaces);
+router.post('/', controller.create);
+router.put('/:id', controller.update);
+router.patch('/:id', controller.update);
+router.delete('/:id', controller.destroy);
+
+module.exports = router;
